612197c656beb3757e687694661d4a3f28c6862b
Wendel Scardua <wendelscardua@gmail.com>
2022-08-09 22:10:17 -0300

    Refactor parsing, add global settings semantics

--- lib/famitracker_parser/song.rb
+++ lib/famitracker_parser/song.rb
@@ -2,66 +2,44 @@
 
 module FamitrackerParser
   class Song
-    attr_reader :export_description,
-                :song_information,
-                :song_comment,
-                :global_settings
-
-    def initialize(export_description:,
-                   song_information:,
-                   song_comment:,
-                   global_settings:)
-      @export_description = export_description
-      @song_information = song_information
-      @song_comment = song_comment
-      @global_settings = global_settings
-    end
+    attr_accessor :export_description,
+                  :song_information,
+                  :song_comment,
+                  :global_settings
   end
 
   class ExportDescription
-    attr_reader :program, :version
-
-    def initialize(program:, version:)
-      @program = program
-      @version = version
-    end
+    attr_accessor :program, :version
   end
 
   class SongInformation
-    attr_reader :title, :author, :copyright
-
-    def initialize(title:, author:, copyright:)
-      @title = title
-      @author = author
-      @copyright = copyright
-    end
-  end
-
-  class SongComment
-    attr_reader :comment
-
-    def initialize(comment:)
-      @comment = comment
-    end
+    attr_accessor :title, :author, :copyright
   end
 
   class GlobalSettings
-    attr_reader :machine,
-                :framerate,
-                :expansion,
-                :vibrato,
-                :split
+    attr_accessor :machine,
+                  :framerate,
+                  :expansion,
+                  :vibrato,
+                  :split,
+                  :n163_channels
 
-    def initialize(machine:,
-                   framerate:,
-                   expansion:,
-                   vibrato:,
-                   split:)
-      @machine = machine
-      @framerate = framerate
-      @expansion = expansion
-      @vibrato = vibrato
-      @split = split
+    def expansion_description
+      case @expansion
+      when 0 then "NES channels only"
+      when 1 then "Konami VRC6"
+      when 2 then "Konami VRC7"
+      when 4 then "Nintendo FDS sound"
+      when 8 then "Nintendo MMC5"
+      when 16 then "Namco 163"
+      end
+    end
+
+    def vibrato_description
+      case @vibrato
+      when 0 then "Old style (bend up)"
+      when 1 then "New style (bend up & down)"
+      end
     end
   end
 end
